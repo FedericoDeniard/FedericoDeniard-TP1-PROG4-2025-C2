11bd71a6872d6d8933b44636d842d175fe0e976c
Federico <fededeniard@gmail.com>
2025-08-21 00:17:41 -0300

    Header con tailwind css

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
   standalone: true
 })
 export class Header {
   private _count = 1
+  private _selected = 'home';
 
   get count(): number {
     return this._count;
@@ -21,4 +23,17 @@ export class Header {
   decrement(): void {
     this._count--;
   }
+
+  get selected(): string {
+    return this._selected;
+  }
+
+  setSelected(selected: string) {
+    this._selected = selected;
+  }
+
+  isSelected(linkName: string): boolean {
+    return this._selected === linkName;
+  }
+
 }
