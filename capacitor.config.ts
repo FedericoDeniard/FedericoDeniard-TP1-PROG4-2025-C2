ec90e9b68c95ee8bd58d140c5feebfdeb883fc8d
Federico <fededeniard@gmail.com>
2025-08-24 12:11:54 -0300

    assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'tp1',
-  webDir: 'dist/tp1/browser'
+  webDir: 'dist/tp1/browser',
+  includePlugins: ['@capacitor/assets'],
+  plugins: {
+    CapacitorAssets: {
+      copyPublicAssets: true
+    }
+  }
 };
 
 export default config;
