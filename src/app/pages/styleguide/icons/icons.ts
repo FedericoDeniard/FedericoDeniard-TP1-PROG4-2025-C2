dd62aef22865d8ab3b986b48faaf49a1fc38ea9c
Federico <fededeniard@gmail.com>
2025-08-24 15:11:59 -0300

    new icons

--- src/app/pages/styleguide/icons/icons.ts
+++ src/app/pages/styleguide/icons/icons.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { featherHome, featherTrash, featherX, featherCheckCircle, featherAlertTriangle, featherInfo, featherLink, featherUsers, featherSettings, featherMail } from '@ng-icons/feather-icons';
 
 @Component({
   selector: 'app-icons',
-  imports: [],
+  standalone: true,
+  imports: [NgIcon],
   templateUrl: './icons.html',
-  styleUrl: './icons.css'
+  styleUrl: './icons.css',
+  viewProviders: [provideIcons({ featherHome, featherTrash, featherX, featherCheckCircle, featherAlertTriangle, featherInfo, featherLink, featherUsers, featherSettings, featherMail })]
 })
 export class Icons {
 
