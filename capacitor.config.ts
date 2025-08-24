1b524fa6b803c1a79d26e051c9ba0ce6f73e510f
Federico <fededeniard@gmail.com>
2025-08-24 11:02:38 -0300

    android instalation

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'tp1',
+  webDir: 'dist/tp1/browser'
+};
+
+export default config;
