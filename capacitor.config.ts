e6569fea594e1b73c1dbf4ec9af70c4fe2d4fd70
Federico <fededeniard@gmail.com>
2025-08-30 10:59:42 -0300

    Splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,7 +8,29 @@ const config: CapacitorConfig = {
   plugins: {
     CapacitorAssets: {
       copyPublicAssets: true
+    },
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 300,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true
     }
+  },
+  android: {
+    // Configuración específica para Android
+    allowMixedContent: true,
+    captureInput: true,
+    webContentsDebuggingEnabled: false
   }
 };
 
